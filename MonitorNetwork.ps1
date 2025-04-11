e2936f6108cd34a624e47812a2f628060eb2e400
Tools 4 Windows 🤩 <velodominator@gmail.com>
2025-04-11 23:06:56 +0200

    Add files via upload

--- MonitorNetwork.ps1
+++ MonitorNetwork.ps1
@@ -1,6 +1,6 @@
 # Parameters
 param(
-    [switch]$r
+    [switch]$runCleanup
 )
 
 # Check for admin permissions
@@ -8,7 +8,7 @@ $AdminScriptURL = "https://raw.githubusercontent.com/tools4windows/other/refs/he
 Invoke-Expression (New-Object Net.WebClient).DownloadString($AdminScriptURL)
 
 # Daily Cleanups Tools4Windows
-if ($r) {
+if ($runCleanup) {
     $AddDailyCleanupsURL = "https://raw.githubusercontent.com/tools4windows/other/refs/heads/main/add-daily-cleanups.ps1"
     Invoke-Expression (New-Object Net.WebClient).DownloadString($AddDailyCleanupsURL)
 }
